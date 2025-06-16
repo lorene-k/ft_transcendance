e18250e0406a10a605bad866caa52774961aad4f
Lorene <lorenejulie@hotmail.com>
2025-06-16 18:54:11 +0200

    added show password toggle

--- frontend/src/routes/loginForm.ts
+++ frontend/src/routes/loginForm.ts
@@ -2,15 +2,29 @@ import { setFormSuccess, setFormError } from "../utils/error.ts";
 import { renderSignupForm } from "./signupForm.ts";
 import { setContent } from "../utils/layout.ts";
 import { handleLogin } from "../app.ts";
+import { handleClickListeners } from "../utils/handlers.ts";
 
-function testLogin() {
-  // ! TEST
+function testLogin() { // ! TEST
   const username = document.getElementById("username") as HTMLInputElement;
   const password = document.getElementById("password") as HTMLInputElement;
   username.value !== "test" ? setFormError(username, "Invalid username") : setFormSuccess(username);
   password.value !== "12345666" ? setFormError(password, "Invalid password") : setFormSuccess(password);
   if (username.value === "test" && password.value === "12345666") 
     handleLogin();
+}
+
+function showPassword() {
+  handleClickListeners("show-password-icon", () => {
+    let icon = document.getElementById("show-password-icon") as HTMLImageElement;
+    let inp = document.getElementById("password") as HTMLInputElement;
+    if (inp && icon && inp.type === "password") {
+      inp.type = "text";
+      icon.src = "/assets/eye-open.png";
+    } else if (inp && icon) {
+      inp.type = "password";
+      icon.src = "/assets/eye-closed.png";
+    }
+  });
 }
 
 // Test - replace testLogin() handleLogin && add API call
@@ -25,4 +39,5 @@ export async function renderLoginForm() {
   signupBtn?.addEventListener("click", () => {
     renderSignupForm();
   });
+  showPassword();
 }
